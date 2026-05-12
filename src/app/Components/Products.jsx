19c7053bd88de132aa0fe15f3664f0272e60f211
ruhul2003 <ruhul941020@gmail.com>
2026-05-13 02:15:54 +0600

    Added product page

---
+++ src/app/Components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <p>THE ULTIMATE FURNITURE EDIT</p>
+            <h1>CURATED PICKS TO ELEVATE</h1>
+        </div>
+    );
+};
+
+export default Products;
