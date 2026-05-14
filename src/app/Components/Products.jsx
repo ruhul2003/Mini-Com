a39cc1481d000fb61c46c8f6b92d1188dd935b6a
ruhul2003 <ruhul941020@gmail.com>
2026-05-15 03:28:36 +0600

    Added design to heading

--- src/app/Components/Products.jsx
+++ src/app/Components/Products.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Products = () => {
     return (
         <div>
-            <p>THE ULTIMATE FURNITURE EDIT</p>
-            <h1>CURATED PICKS TO ELEVATE</h1>
+            <p className="text-2xl font-semibold"   >THE ULTIMATE FURNITURE EDIT</p>
+            <h1 className="text-3xl font-bold" >CURATED PICKS TO ELEVATE</h1>
         </div>
     );
 };
